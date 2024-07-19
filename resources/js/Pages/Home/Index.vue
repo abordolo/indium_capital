b83c8bb4c8cb489f581f28a4d8059e7ee3bd73e9
abordolo <anupam.assam@gmail.com>
2024-07-20 01:16:14 +0530

    skeleton for hero section is created

--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -1,11 +1,22 @@
 <template>
-  <!-- contents wrapper -->
-  <div>Home Page</div>
-  <!-- contents wrapper -->
+  <!-- page wrapper -->
+  <div>
+    <!-- meta -->
+    <InertiaHead>
+      <title>Home</title>
+    </InertiaHead>
+    <!-- meta -->
+
+    <!-- contents -->
+    <Hero />
+    <!-- contents -->
+  </div>
+  <!-- page wrapper -->
 </template>
 
 <script setup>
 // imports
+import Hero from './Partials/Hero.vue';
 
 // props
 </script>
---
+++ resources/js/Pages/Home/Partials/Hero.vue
@@ -0,0 +1,15 @@
+<template>
+  <Section>
+    <Container>
+      <!-- contents -->
+      <div>Hero</div>
+      <!-- contents -->
+    </Container>
+  </Section>
+</template>
+
+<script setup>
+// imports
+
+// props
+</script>
