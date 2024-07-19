ace24cd1edf2e52b52d879accdf95231dc028509
abordolo <anupam.assam@gmail.com>
2024-07-20 00:57:59 +0530

    updated tailwindcss config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,39 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
-    ],
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+    './resources/js/**/*.vue',
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+
+      screens: {
+        xs: '480px',
+      },
+
+      colors: {
+        primary: {
+          50: '#E0F7FF',
+          100: '#C4FFFF',
+          200: '#A8FFFF',
+          300: '#8CFFFF',
+          400: '#70EDFF',
+          500: '#007DC5', // primary
+          600: '#54D1FF',
+          700: '#38B5FD',
+          800: '#1C99E1',
+          900: '#007DC5',
         },
+      },
     },
+  },
 
-    plugins: [forms],
+  plugins: [forms],
 };
